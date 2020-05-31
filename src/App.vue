960e3faff9ca0407b9426dec6bad61e14a179424
tareq <taregh1996nazari@gmail.com>
2020-05-31 07:00:26 -0700

    test

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,24 @@
 
 <script>
 
-  import SearchBox from "./components/SearchBox";
+
   import WeeklyBox from "./components/WeeklyBox";
-<<<<<<< HEAD
+
   import Footer from "./components/Footer";
   import Nav from "./components/Nav";
-=======
 
->>>>>>> 3c898caf26dc3151fb3a6fe04ee81837f388f7
+
+
 export default {
 
   components: {
     SearchBox,
     WeeklyBox,
-<<<<<<< HEAD
+
     Footer,
     Nav,
-=======
 
->>>>>>> 3c898caf2286dc3151fb3a6fe04ee81837f388f7
+
   }
 
 }
---
+++ src/components/StorePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+
+
+
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "SearchBox"
+
+    }
+</script>
+
+
+<style scoped>
+.container {
+
+}
+  .header {
+
+  }
+
+
+</style>
